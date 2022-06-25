6760f69eb33ef8c299ba1205bb70cc2afdb58620
Syderi <syderi@yahoo.com>
2022-06-25 18:16:58 +0300

    init: add js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function reverse (n) {
-  
+// module.exports = function reverse(n) {
+module.exports = function reverse(n) {
+    // console.log(n)
+    n < 0 ? argum = -1 : argum = 1;
+    let n1 = n*argum;
+    let n2 = n1 + '';
+    // console.log(n1);
+    // console.log(n1.split('').reverse().join(''));
+    let n3 = n2.split('').reverse().join('');
+    let n4= n3 *1 / 1;
+    // console.log(n3);
+    console.log(typeof(n4));
+    return n4*argum;
 }
+
+console.log(module.exports(-231));
